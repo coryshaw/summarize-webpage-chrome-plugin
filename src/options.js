4ba5766ef7aba40d25edd046659288cf6217b1d1
Cory Shaw <coryshaw@gmail.com>
2024-05-24 13:52:49 -0600

    chore: cleanup unused files

--- src/options.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("save").addEventListener("click", function () {
-  var apiKey = document.getElementById("apiKey").value;
-  localStorage.setItem("apiKey", apiKey);
-  alert("API Key saved!");
-});
