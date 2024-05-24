09d39595e5a23df86c5cc06a4d322f1910730be7
Cory Shaw <coryshaw@gmail.com>
2024-05-24 09:55:30 -0600

    dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { MantineProvider, Anchor, Container } from "@mantine/core";
+import { MantineProvider, Container, Divider, Button } from "@mantine/core";
 import APIKeyForm from "./components/APIKeyForm";
 import "@mantine/core/styles.css";
 import Summarizer from "./components/Summarizer";
+import { IconSettings } from "@tabler/icons-react";
 
 function App() {
   const [apiKey, setApiKey] = useState(null);
   const [displayAPIKeyForm, setDisplayAPIKeyForm] = useState(false);
+  const [localColorScheme, setLocalColorScheme] = useState("dark");
 
   useEffect(() => {
+    // get api key
     chrome.storage.local.get("apiKey", function (data) {
       if (data.apiKey) {
         setApiKey(data.apiKey); // Pre-populate the field
@@ -17,17 +20,43 @@ function App() {
         setDisplayAPIKeyForm(true);
       }
     });
+
+    // detect theme
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (event) => {
+      setLocalColorScheme(event.matches ? "dark" : "light");
+    };
+
+    if (mediaQuery.matches) {
+      setLocalColorScheme("dark");
+    }
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
   }, []);
 
   return (
-    <MantineProvider>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      forceColorScheme={localColorScheme}
+    >
       <Container miw={550} ml={10} mt={20} mr={10} mb={20}>
         {!displayAPIKeyForm && apiKey && (
           <>
             <Summarizer />
-            <Anchor href="#" onClick={() => setDisplayAPIKeyForm(true)}>
-              Change API Key
-            </Anchor>
+            <Divider mt={20} mb={20} />
+            <Button
+              leftSection={<IconSettings size={14} />}
+              variant="default"
+              href="#"
+              onClick={() => setDisplayAPIKeyForm(true)}
+            >
+              Settings
+            </Button>
           </>
         )}
 
--- src/components/Summarizer.jsx
+++ src/components/Summarizer.jsx
@@ -1,47 +1,62 @@
-import { Loader, Container, Alert } from "@mantine/core";
+import { Loader, Alert, Flex, ActionIcon } from "@mantine/core";
 import React, { useState, useEffect } from "react";
 import { extractPageText } from "../utils/extractContent";
 import { fetchSummary } from "../utils/fetchSummary";
-import { IconAlertCircle } from "@tabler/icons-react";
+import { IconRefresh } from "@tabler/icons-react";
 
 function Summarizer() {
   const [loading, setLoading] = useState(true);
   const [summary, setSummary] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState("Error!");
+
+  const summarizeContent = async () => {
+    setLoading(true);
+    setError(null);
+    setSummary(null);
+    try {
+      const extractedText = await extractPageText();
+      if (!extractedText) {
+        setError("No content available.");
+        setLoading(false);
+        return;
+      }
+      const fetchedSummary = await fetchSummary(extractedText);
+      const formattedSummary = marked.parse(fetchedSummary);
+      setSummary(formattedSummary);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   // on load, extract page text and fetch summary
   useEffect(() => {
-    const summarizeContent = async () => {
-      try {
-        const extractedText = await extractPageText();
-        if (!extractedText) {
-          setError("No content available.");
-          setLoading(false);
-          return;
-        }
-        const fetchedSummary = await fetchSummary(extractedText);
-        const formattedSummary = marked.parse(fetchedSummary);
-        setSummary(formattedSummary);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     summarizeContent();
   }, []);
 
   return (
-    <Container>
-      {loading && <Loader size="lg" />}
-      {error && (
-        <Alert variant="light" color="red" title={error} icon={IconAlertCircle}>
-          {error}
-        </Alert>
+    <div>
+      {loading && (
+        <Flex justify="center" align="center" style={{ height: "100%" }}>
+          <Loader type="dots" color="pink" />
+        </Flex>
       )}
-      {summary && <div dangerouslySetInnerHTML={{ __html: summary }} />}
-    </Container>
+      {error && <Alert variant="light" title={error} color="red" />}
+      {summary && (
+        <>
+          <ActionIcon
+            style={{ position: "absolute", top: "20px", right: "20px" }}
+            onClick={() => summarizeContent()}
+            variant="default"
+            aria-label="Settings"
+          >
+            <IconRefresh style={{ width: "70%", height: "70%" }} stroke={1.5} />
+          </ActionIcon>
+          <div dangerouslySetInnerHTML={{ __html: summary }} />
+        </>
+      )}
+    </div>
   );
 }
 
